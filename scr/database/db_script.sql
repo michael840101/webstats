2adade06aa871bf63fe8ae4493ee5a552d468e5f
Zhi <michael840101@yahoo.com>
2019-02-06 02:44:52 +0000

    spark processing modified

--- scr/database/db_script.sql
+++ scr/database/db_script.sql
@@ -1,6 +1,30 @@
 --PostgreSQL 9.5.14 on x86_64-pc-linux-gnu
 --sql scripts for setting up the cc_dev database
 
+--CREATE MAIN DOMAIN TABLE
+CREATE  TABLE IF NOT EXISTS domains (
+  domain character varying(128),
+  tld character varying(64),
+  url_total int,
+	url_increased int,
+	url_decreased int,
+  total_content int,
+	lang_code_0 character varying(36),
+  lang_code_1 character varying(36),
+  stats_date timestamp
+);
+
+--CREATE  language and distribution table
+CREATE  TABLE IF NOT EXISTS languages (
+lang_code character varying(32),
+language  character varying(128),
+country  character varying(128),
+population int
+);
+
+
+
+--CREATE AND PRE LOAD THE LOCATION OF IP ADDRESS TABLE
 CREATE TABLE ip2location(
 	ip_from bigint NOT NULL,
 	ip_to bigint NOT NULL,
@@ -15,11 +39,12 @@ CREATE TABLE ip2location(
 	CONSTRAINT ip2location_pkey PRIMARY KEY (ip_from, ip_to)
 );
 
-sudo -u postgres psql postgres
-COPY ip2location FROM '/home/ubuntu/IP2LOCATION-LITE-DB11.CSV' WITH CSV QUOTE AS '"';
+--sudo -u postgres psql postgres
+--COPY ip2location FROM '/home/ubuntu/IP2LOCATION-LITE-DB11.CSV' WITH CSV QUOTE AS '"';
 
 
------Create cc_url TABLE
+
+-----Plan B:  Create cc_url TABLE ---------
 CREATE  TABLE IF NOT EXISTS cc_url (
   url  character varying(256),
   domain character varying(128),
--- scr/spark/cc_url_filter.py
+++ scr/spark/cc_url_filter.py
@@ -1,33 +1,35 @@
 import os
-import json
-import logging
 from pyspark import SparkConf, SparkContext
 from pyspark.sql import SQLContext, SparkSession
+from pyspark.sql.window import Window
+from pyspark.sql.functions import *
+from pyspark.sql.types import *
 import boto
 from boto.s3.key import Key
-from gzipstream import GzipStreamFile
-from pyspark.sql.types import *
 from datetime import datetime
-import warc
-import tldextract as te
 
 
-
-
-#log_level = 'INFO'
-#logging.basicConfig(level=log_level, format=LOGGING_FORMAT)
 
 conf = SparkConf()
 sc = SparkContext(conf = conf)
 sqlContext = SQLContext(sparkContext=sc)
 
-# spark = SparkSession \
-#     .builder \
-#     .appName("Python Spark SQL basic example") \
-#     .config("spark.some.config.option", "some-value") \
-#     .getOrCreate()
+#s3 setting
+awsAccessKeyId=os.environ.get('AWS_ACCESS_KEY_ID')
+awsSecretAccessKey=os.environ.get('AWS_SECRET_ACCESS_KEY')
+sc._jsc.hadoopConfiguration().set('fs.s3n.awsAccessKeyId',awsAccessKeyId)
+sc._jsc.hadoopConfiguration().set('fs.s3n.awsSecretAccessKey',awsSecretAccessKey)
+sc._jsc.hadoopConfiguration().set('fs.s3.endpoint','s3.us-east-1.amazonaws.com')
+sc._jsc.hadoopConfiguration().set('fs.s3.impl','org.apache.hadoop.fs.s3native.NativeS3FileSystem')
 
+#PostgreSQL connection setting
+db_uer=os.environ.get('DB_USER')
+db_password=os.environ.get('DB_PASS')
+mode = "append"
+url_connect = "jdbc:postgresql://ec2-3-92-151-139.compute-1.amazonaws.com:5432/cc_dev"
+properties = {"user": db_uer,"password": db_password,"driver": "org.postgresql.Driver"}
 
+#output df table structure
 schema = StructType([
     StructField("url", StringType(), True),
     StructField("domain", StringType(), True),
@@ -39,31 +41,41 @@ schema = StructType([
 ])
 
 
-#s3 setting
-awsAccessKeyId=os.environ.get('AWS_ACCESS_KEY_ID')
-awsSecretAccessKey=os.environ.get('AWS_SECRET_ACCESS_KEY')
-sc._jsc.hadoopConfiguration().set('fs.s3n.awsAccessKeyId',awsAccessKeyId)
-sc._jsc.hadoopConfiguration().set('fs.s3n.awsSecretAccessKey',awsSecretAccessKey)
-sc._jsc.hadoopConfiguration().set('fs.s3.endpoint','s3.us-east-1.amazonaws.com')
-sc._jsc.hadoopConfiguration().set('fs.s3.impl','org.apache.hadoop.fs.s3native.NativeS3FileSystem')
-
-df_input = sqlContext\
+#read from parquet to df
+df_current_month = sqlContext\
             .read.parquet("s3a://insightdemozhi/cc/*")
+df_previouse_month = sqlContext\
+            .read.parquet("s3a://insightdemozhi/cc_test/*")
 
-#df_output.filter(df_input['tld'] = 'com')
-df_input.createOrReplaceTempView("url_temp")
-df_output = sqlContext.sql("SELECT * FROM url_temp where tld='com'")
+#create temp table from df
+df_current_month.createOrReplaceTempView("urls_curr_mon")
+df_previouse_month.createOrReplaceTempView("urls_pre_mon")
+
+#query for creating output df
+df_domain_url_total = sqlContext.sql("SELECT domain, count(url) as ct\
+                        FROM urls_curr_mon \
+                        GROUP BY domain")
+
+df_domain_url_increase = sqlContext.sql("SELECT domain, count(url)\
+                        FROM urls_curr_mon cm \
+                        WHERE cm.url NO IN \
+                        (SELECT url FROM urls_pre_mon)\
+                        GROUP BY domain )
+
+df_domain_url_decrease = sqlContext.sql("SELECT domain, count(url)\
+                        FROM urls_pre_mon pm \
+                        WHERE pm.url NO IN \
+                        (SELECT url FROM urls_curr_mon)\
+                         GROUP BY domain ))
+
+#calculate rank for page counts using window functions
+ranked =  df_domain_url_total.withColumn(\
+  "ct_rank", dense_rank().over(Window.partitionBy("domain").orderBy(desc("ct"))))
+
+#Join df for the output table
+#Date
 
 
-
-
-
-#PostgreSQL connection setting
-db_uer=os.environ.get('DB_USER')
-db_password=os.environ.get('DB_PASS')
-mode = "overwrite"
-url_connect = "jdbc:postgresql://ec2-3-92-151-139.compute-1.amazonaws.com:5432/cc_dev"
-properties = {"user": db_uer,"password": db_password,"driver": "org.postgresql.Driver"}
 
 #Write the output data frame to PostgreSQL database
 df_output.write \
